569ccfc76f0772468cdfb10e944bf1ba097fe4b0
User <Loclhse172459@fpt.edu.vn>
2025-08-29 13:44:08 +0700

    done

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,6 @@ export const API_BASE_URL = isDevelopment
   ? 'http://103.90.227.18'  // Use production API for now
   : 'http://103.90.227.18';
 
-export const WS_BASE_URL = API_BASE_URL;
+export const WS_BASE_URL = "http://103.90.227.18:8081";
 
 
