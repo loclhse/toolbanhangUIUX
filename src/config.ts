bbd64d88ddc563bfdf83ec866844b44b368c0aae
sometimesithardtofindpeace <160689025+loclhse@users.noreply.github.com>
2025-08-30 21:14:43 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,7 @@
 export const API_BASE_URL = 'http://103.90.227.18:8080';
 
 // WebSocket base URLs
-export const WS_BASE_URL = [
-  "http://103.90.227.18:8081",
-  "http://103.90.227.18:82"
-];
+export const WS_BASE_URL =  "http://103.90.227.18:8080",
+
 
 
